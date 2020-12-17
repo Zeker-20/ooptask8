2d807365c30b3680354ce8e14037f93fbafbbde7
Zeker-20 <71704841+Zeker-20@users.noreply.github.com>
2020-12-17 17:50:08 +0800

    Add files via upload

---
+++ arrays.h
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef __ARRAYS_H__
+#define __ARRAYS_H__
+
+class MyArray
+{
+
+private:
+	int *ps;
+	int size;
+
+public:
+
+	MyArray(int _size = 20);
+	MyArray(const MyArray &r);
+	~MyArray();
+
+	void show();
+	MyArray& modifyArray();
+
+	MyArray& operator=(const MyArray &r);
+	MyArray operator+(const MyArray &r);
+
+	friend MyArray operator-(MyArray &r1, MyArray &r2);
+
+};
+
+
+#endif
